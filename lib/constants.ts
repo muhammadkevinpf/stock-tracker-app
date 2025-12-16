ded4f55c81d97bb2e0a2719257e58d012165788c
Muhammad Kevin <muhammad.kevin@prudential.co.id>
2025-12-16 14:13:49 +0700

    implement header

---
+++ lib/constants.ts
@@ -0,0 +1,10 @@
+interface navItems {
+    href: string;
+    label: string;
+}
+
+export const NAV_ITEMS: navItems[] = [
+    { href: '/', label: 'Dashboard'},
+    { href: '/search', label: 'Search'},
+    { href: '/watchlist', label: 'Watchlist'},
+]
